84a424bd88c918003c488e19a0f525c12379d9b5
vijayhardaha <vijayhardaha@gmail.com>
2023-10-20 15:03:11 +0530

    feat(add): add touch events for mobile devices

--- play-pause-video-on-hover/assets/js/script.js
+++ play-pause-video-on-hover/assets/js/script.js
@@ -2,29 +2,54 @@
 const videoCurrentTimes = {};
 
 // Get all video elements
-const videos = document.querySelectorAll('.video');
+const videos = document.querySelectorAll(".video");
 
-// Add event listeners to each video container
-videos.forEach((video) => {
-	const container = video.closest('.video-container');
-	const imageLayer = container.querySelector('.image-layer');
-
-	video.muted = true;
-	video.volume = 0;
-	video.setAttribute('disableRemotePlayback', 'true');
-	video.setAttribute('disablePictureInPicture', 'true');
-
-	container.addEventListener('mouseenter', () => {
+// Define a function to handle video interactions
+function handleVideoInteraction(video, imageLayer) {
+	return () => {
 		if (videoCurrentTimes[video.id]) {
 			video.currentTime = videoCurrentTimes[video.id];
 		}
 		video.play();
-		imageLayer.style.opacity = 0; // Change image opacity to 0 on hover
-	});
+		imageLayer.style.opacity = 0; // Change image opacity to 0 on interaction
+	};
+}
 
-	container.addEventListener('mouseleave', () => {
+// Define a function to handle the end of video interactions
+function handleVideoInteractionEnd(video, imageLayer) {
+	return () => {
 		videoCurrentTimes[video.id] = video.currentTime;
 		video.pause();
-		imageLayer.style.opacity = 1; // Restore image opacity on mouse leave
-	});
+		imageLayer.style.opacity = 1; // Restore image opacity on interaction end
+	};
+}
+
+// Add event listeners to each video container
+videos.forEach((video) => {
+	const container = video.closest(".video-container");
+	const imageLayer = container.querySelector(".image-layer");
+
+	video.muted = true;
+	video.volume = 0;
+	video.setAttribute("disableRemotePlayback", "true");
+	video.setAttribute("disablePictureInPicture", "true");
+
+	container.addEventListener(
+		"mouseenter",
+		handleVideoInteraction(video, imageLayer)
+	);
+	container.addEventListener(
+		"mouseleave",
+		handleVideoInteractionEnd(video, imageLayer)
+	);
+
+	// Add touch event listeners for touch devices
+	container.addEventListener(
+		"touchstart",
+		handleVideoInteraction(video, imageLayer)
+	);
+	container.addEventListener(
+		"touchend",
+		handleVideoInteractionEnd(video, imageLayer)
+	);
 });
